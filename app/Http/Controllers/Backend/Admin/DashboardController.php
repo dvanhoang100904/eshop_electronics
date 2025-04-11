91f1c9185d83bb68157293d9942fde85474212c8
dvanhoang100904 <daovanhoang100904@gmail.com>
2025-04-11 08:22:04 +0700

    update dashboard

--- app/Http/Controllers/Backend/Admin/DashboardController.php
+++ app/Http/Controllers/Backend/Admin/DashboardController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Backend\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('backend.dashboard.dashboard');
+        $userCount = User::Count();
+        return view('backend.dashboard.dashboard', compact('userCount'));
     }
 }
